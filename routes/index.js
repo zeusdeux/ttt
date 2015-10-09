7e44bda88ce7bfbb16be0842031962da0f2b1db4
Mudit Ameta <zeusdeux@gmail.com>
2015-10-10 00:25:22 +0530

    Switched gameplay logic to return the board with o at a random position

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,25 @@ var boardUtils = require('../utils/boardUtils');
 
 
 /**
-* GET home page.
+* GET next tic tac toe move
 */
 
 router.get('/', function(req, res) {
   var board = req.query.board;
+  var randomWhiteSpaceIndex;
 
   // Validate board
   if (boardUtils.validateBoard(board)) {
-    // flip first occurrence of whitespace to o
-    board = board.replace(' ', 'o');
+    // get the index of a random white space on the board
+    randomWhiteSpaceIndex = boardUtils.getRandomWhiteSpaceIndex(board);
+
+    // flip the whitespace at `randomWhiteSpaceIndex` to o
+    board = [].reduce.call(board, function(accumulator, currChar, index) {
+      if (index === randomWhiteSpaceIndex) accumulator += 'o';
+      else accumulator += currChar;
+      return accumulator;
+    }, '');
+
     res.end(board);
   }
   else res.sendStatus(400);
